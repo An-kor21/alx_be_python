66ac9f2a191e7ed527250cf29022451ac1c35bd0
Osai An-kor <ankorsydney2003@gmail.com>
2025-09-16 23:32:46 +0100

    Add 3 more arithmetic operations

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+current_age = int(input("How old are you? "))
+future_years = current_age + 27
+age = future_years
+print("In 2050, you will be", age, "years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+conversion = hours * 3600
+seconds = conversion
+print([hours], "hour(s)", "is", [seconds], "seconds")
