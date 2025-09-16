f50bec4ac03da8992ec263b98f447f86a400f33e
Osai An-kor <ankorsydney2003@gmail.com>
2025-09-16 21:12:15 +0100

    Reorder directories and files to the original intended path

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
 sum = number1 + number2
-print("Addition of 10 and 15 is", sum)
+print("Addition of 10 and 5 is", sum)
 difference = number1 - number2
 print("Subtraction of 10 and 5 is", difference)
 product = number1 * number2
