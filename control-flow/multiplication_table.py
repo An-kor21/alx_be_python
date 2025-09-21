368fe8b11a819a7bb331d48dff710f1b2eacaf84
Osai An-kor <ankorsydney2003@gmail.com>
2025-09-21 21:21:00 +0100

    Use a for loop to generate and print the multiplication table for a given number.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
     result = i * number
-    print(f"{number} x {i} = {result}")
+    #print(f"{number} x {i} = {result}")
+    print(number, "*", i, "=", result)
